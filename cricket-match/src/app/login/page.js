de49263647bee6f8c50bcf92278fd7c6b318be3c
Affan Amir <affanamir290@gmail.com>
2023-11-24 14:53:51 +0500

    latest

--- cricket-match/src/app/login/page.js
+++ cricket-match/src/app/login/page.js
@@ -1,66 +1,102 @@
 "use client"
 
-import styles from   "./login.module.css"
-import { redirect } from "next/navigation";
-import Link from "next/link";
+import styles from "./login.module.css";
 import { useState } from "react";
 import NavBar from "../components/shared/NavBar/NavBar";
+import Link from "next/link";
 
 function Login() {
-
-
   const [login, setLogin] = useState({
-    Username:"",
-    Password:""
-  })
+    Username: "",
+    Password: "",
+  });
 
-  const Change = (e)=>{
-     setLogin({...login,[e.target.name]:e.target.value})
-  }
+  const Change = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
 
-  const handleClick =async (e) =>{
+  const handleClick = async (e) => {
     e.preventDefault();
-    console.log(login);
-    let response = await fetch("http://localhost:5005/api/auth/login",{
-      method:'post',
-      headers:{
-        'content-type':'application/json', 
-      },
-      body:JSON.stringify({
-        Username : login.Username ,
-        Password: login.Password
-      })
-    });
-    response = await response.json();
 
-    console.log(response);
+    try {
+      const response = await fetch("http://localhost:5005/api/auth/login", {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Username: login.Username,
+          Password: login.Password,
+        }),
+      });
 
-    if (response.token) {
-      localStorage.setItem("login",JSON.stringify(response));
-      window.location.href="/";
-      
+      // Log raw response for troubleshooting
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      console.log(responseData);
+
+      if (responseData.token) {
+        localStorage.setItem("login", JSON.stringify(responseData));
+        window.location.href = "/";
+      } else {
+        console.error("Login failed. No token in the response.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error.message);
     }
-  }
+  };
 
   return (
     <div>
       <NavBar />
-    <div className='container flex justify-center items-center'  style={{height:"100vh"}}> 
-    <div className="form-floating mb-3 ">
-  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name="Username" onChange={Change} />
-  <label htmlFor="floatingInput" className={styles.main}>Email address</label>
-</div>
-<br></br>
-<div className="form-floating">
-  <input type="password" className="form-control" id="floatingPassword" placeholder="Password" name="Password" onChange={Change}/>
-  <label htmlFor="floatingPassword" >Password</label>
-</div>
-<br></br>
-<button className=' text-center bg-slate-400' onClick={handleClick}>Submit</button>
-<Link href={"/register"}> Registration</Link>
+      <div
+        className={`container flex justify-center items-center ${styles.loginContainer}`}
+        style={{ height: "100vh" }}
+      >
+        <div className={`form-floating mb-3 ${styles.formElement}`}>
+          <input
+            type="email"
+            className={`form-control ${styles.input}`}
+            id="floatingInput"
+            placeholder="name@example.com"
+            name="Username"
+            onChange={Change}
+          />
+          <label htmlFor="floatingInput" className={`${styles.label} ${styles.main}`}>
+            Email address
+          </label>
+        </div>
+        <br />
+        <div className={`form-floating ${styles.formElement}`}>
+          <input
+            type="password"
+            className={`form-control ${styles.input}`}
+            id="floatingPassword"
+            placeholder="Password"
+            name="Password"
+            onChange={Change}
+          />
+          <label htmlFor="floatingPassword" className={styles.label}>
+            Password
+          </label>
+        </div>
+        <br />
+        <button
+          className={`text-center ${styles.button}`}
+          onClick={handleClick}
+        >
+          Submit
+        </button>
+        <Link href="/register"> Registration</Link>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
+
