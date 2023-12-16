621ab6f149f1a68e767ea4f909cfc9522a002502
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-12-16 17:24:46 +0500

    Matches Routes: 'All api have been made and tested successfully'

--- backend/routers/matchEdit/matchEdit.js
+++ backend/routers/matchEdit/matchEdit.js
@@ -34,9 +34,10 @@ async(req,res)=>{
     });
 
     const venue_id = venue.insertId;
-    
+    console.log(venue_id);
+
     const result = await querySql({
-        query: "INSERT INTO matches (TournamentName, StartingDate, EndingDate, AddingDate,ImageUrl) VALUES (?, ?, ?, ?,?)",
+        query: "INSERT INTO matches (tournament_id,venue_id,match_date,match_time, team1, team2, AddingDate,img) VALUES (?, ?, ?, ?,?,?,?,?)",
         values: [tournament_id,venue_id,match_date,match_time, team1, team2, AddingDate,img]
     });
     
@@ -49,13 +50,27 @@ async(req,res)=>{
 })
 
 //Updating Matches
-router.put("/updatetournament",
+router.put("/updatematch",
 async(req,res)=>{
-    const {tournament_id,TournamentName,StartingDate,EndingDate,AddingDate} = req.body
+    const {tournament_id,match_id,match_date,match_time,team1,team2,img,AddingDate,venue_name,location} = req.body
+
+    const venue_id = await querySql({
+        query:"select venue_id from matches where match_id = ? ",
+        values:[match_id]
+    })
 
     const result = await querySql({
-        query: "Update tournament Set TournamentName = ?, StartingDate = ?, EndingDate = ?, AddingDate = ? where tournament_id = ?" ,
-        values: [TournamentName, StartingDate, EndingDate, AddingDate,tournament_id]
+        query: "Update matches Set match_date=?,match_time=?,team1=?,team2=?,img=?,AddingDate=? where tournament_id = ? And match_id = ?",
+        values: [match_date,match_time,team1,team2,img,AddingDate,tournament_id,match_id]
+    });
+
+    console.log(match_id);
+
+    console.log(venue_id[0].venue_id);
+
+    const result2 = await querySql({
+        query: "Update venues Set AddingDate = ?,venue_name = ?,location = ? WHERE venue_id = ?",
+        values: [AddingDate,venue_name,location,venue_id[0].venue_id]
     });
 
     if (!result || result.length === 0) {
@@ -69,20 +84,30 @@ async(req,res)=>{
 
 
 //Deleting Matches
-router.delete("/deletetournament",
+router.delete("/deletematch",
 async(req,res)=>{
-    const {tournament_id} = req.body
+    const {match_id} = req.body
+
+    const venue_id = await querySql({
+        query:"select venue_id from matches where match_id = ? ",
+        values:[match_id]
+    })
 
     const result = await querySql({
-        query: "Delete From tournament where tournament_id = ?" ,
-        values: [tournament_id]
+        query: "Delete From matches where match_id = ?" ,
+        values: [match_id]
+    });
+
+    const result1 = await querySql({
+        query: "Delete From venues where venue_id = ?" ,
+        values: [venue_id[0].venue_id]
     });
 
     if (!result || result.length === 0) {
         res.status(201).json({Msg:"No tournaments are found"})
     }
 
-    res.status(200).json({Msg:"Tournament is deleted"});
+    res.status(200).json({Msg:"Match & its venue is deleted"});
 })
 
 module.exports = router
